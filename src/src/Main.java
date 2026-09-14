99d6b342c2987318ae8d7aeda71ce6ca2f3032cc
kaloginov <loginovkirill90000@gmail.com>
2026-09-14 09:07:14 +0300

    gitignore

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,19 +1,10 @@
 package src;
 
-import university.Student;
-import university.StudentsGroup;
-import university.Trainees;
+import src.university.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Trainees a = new StudentsGroup(1,"ПМиК",1);
-        Student nn = new Student();
-        a.addStudent(nn);
-        a.increaseAvgMark(nn);
-        a.increaseAvgMark(nn);
-
-
-
-        System.out.printf("%s",a);
+        var a = new Student();
+        System.out.println(a);
     }
 }
