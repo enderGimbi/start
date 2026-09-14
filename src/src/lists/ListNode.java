78f3dfad6ac7f78a49d9b4ecf42df85dbc792700
kaloginov <loginovkirill90000@gmail.com>
2026-09-14 09:57:43 +0300

    ListNode

--- src/src/lists/ListNode.java
+++ src/src/lists/ListNode.java
@@ -1,4 +1,42 @@
 package src.lists;
 
-public class ListNode {
+public class ListNode<T1> {
+    Node<T1> head;
+    int length;
+
+    public ListNode() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    public ListNode(Node<T1> head) {
+        this.head = head;
+        this.length=1;
+    }
+
+    public void add(T1 newEl){
+        var cache = head;
+        while(cache.next!=null){
+            cache = cache.next;
+        }
+        cache.next=new Node<T1>(newEl);
+    }
+
+    public void addNode(Node<T1> node){
+        var cache = head;
+        while(cache.next!=null){
+            cache = cache.next;
+        }
+        cache.next=node;
+    }
+
+    public void deleteEl(T1 el){
+        var cache = head;
+        Node<T1> prev;
+        while(cache.next!=null || cache.getValue()==el){
+            prev = cache;
+            cache = cache.next;
+        }
+        if(cache.next==null&&cache.getValue()!=el) return;
+    }
 }
--- src/src/lists/Node.java
+++ src/src/lists/Node.java
@@ -1,5 +1,30 @@
 package src.lists;
 
-public class Node {
+public class Node<T1> {
+     private T1 value;
+     Node<T1> next;
 
+     public Node() {
+         this.value = null;
+         this.next = null;
+     }
+
+    public Node(T1 value) {
+        this.value = value;
+        this.next = new Node<T1>();
+    }
+
+    public Node(T1 value, Node<T1> next) {
+         this.value = value;
+         this.next = next;
+     }
+
+
+    public T1 getValue() {
+        return value;
+    }
+
+    public void setValue(T1 value) {
+        this.value = value;
+    }
 }
